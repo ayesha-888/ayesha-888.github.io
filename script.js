6180adb659cd63817bc4f2cbc18efe458dce4421
ayesha-888 <168366986+ayesha-888@users.noreply.github.com>
2025-08-11 19:19:15 +0000

    modified js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function invertColors() {
   document.body.classList.toggle("inverted");
-
+  
   if (document.body.classList.contains("inverted")) {
     localStorage.setItem("mode", "inverted");
   } else {
     localStorage.setItem("mode", "normal");
   }
-}
+  }
 
-window.onload = function () {
+  window.onload = function () {
   if (localStorage.getItem("mode") === "inverted") {
     document.body.classList.add("inverted");
   }
-}
-document.getElementById("invertBtn").addEventListener("click", invertColors);
+  }
+  document.getElementById("invertBtn").addEventListener("click", invertColors);
